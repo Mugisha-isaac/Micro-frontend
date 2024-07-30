677068c62b0eebffb75be28475cd49f62338add4
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-30 15:43:25 +0200

    ft: add products page

--- main-app/src/pages/index.tsx
+++ main-app/src/pages/index.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="h-[100vh] flex justify-center items-center">
-      Hello main app
+    <main className="flex flex-col bg-white">
+      <Navbar />
+      <div className="h-[75vh] py-4">
+        <h1 className="text-3xl font-bold font-mono h-[100%] flex justify-center items-center text-black">
+          Welcome to the Main Application
+        </h1>
+      </div>
+      <Footer/>
     </main>
   );
 }
--- shop-app/src/pages/index.tsx
+++ shop-app/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Products from "@/components/Products";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="h-[100vh] flex justify-center items-center">
-       Hello shop app
-    </main>
+    <Products/>
   );
 }
